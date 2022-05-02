cd74799497a9554ace21dd7da5e9e2ab7cac8e8c
João Victor de Souza <j.victor@sinova.com.br>
2022-05-02 20:06:41 -0300

    [Update]: atualizado a função 'Start' em script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function Start() {
 	$("#init").hide();
-	$("#gameBackground").append("<div id='player'></div>");
-	$("#gameBackground").append("<div id='enemyOne'></div>");
+	$("#gameBackground").append("<div id='player' class='animationHelicopter'></div>");
+	$("#gameBackground").append("<div id='enemyOne' class='animationEnemyOne'></div>");
 	$("#gameBackground").append("<div id='enemyTwo'></div>");
-	$("#gameBackground").append("<div id='friend'></div>");
+	$("#gameBackground").append("<div id='friend' class='animationFriend'></div>");
 }
